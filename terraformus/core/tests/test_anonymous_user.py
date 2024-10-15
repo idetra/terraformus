1df80741e061fa486155fe32522b8fee96bdbf78
GuiFV <guilhermeviotti@gmail.com>
2024-10-15 12:29:03 +0100

    new tests

--- terraformus/core/tests/test_anonymous_user.py
+++ terraformus/core/tests/test_anonymous_user.py
@@ -22,3 +22,38 @@ class UserReplacementTestCase(TestCase):
 
         self.assertEqual(self.solution.user, anonymous_user)
         self.assertEqual(self.strategy.user, anonymous_user)
+
+
+class UserSetupTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Test', password='Test')
+        self.solution = Solution.objects.create(user=self.user)
+        self.strategy = Strategy.objects.create(user=self.user)
+
+    def test_user_and_objects_creation(self):
+        self.assertIsNotNone(self.user, 'User was not created')
+        self.assertIsNotNone(self.solution, 'Solution was not created')
+        self.assertIsNotNone(self.strategy, 'Strategy was not created')
+        self.assertEqual(self.solution.user, self.user)
+        self.assertEqual(self.strategy.user, self.user)
+
+    def test_user_deletion(self):
+        self.user.delete()
+        self.assertFalse(User.objects.filter(username='Test').exists(), 'User was not deleted')
+
+    def test_anonymous_user_creation(self):
+        self.user.delete()
+        anonymous_user = User.objects.filter(username__startswith='Anonymous-').last()
+        self.assertIsNotNone(anonymous_user, 'Anonymous user was not created')
+
+    def test_objects_update_to_anonymous_user(self):
+        self.user.delete()
+        anonymous_user = User.objects.filter(username__startswith='Anonymous-').last()
+
+        # Reload the objects from the DB to get the updated user relations
+        solution = Solution.objects.get(id=self.solution.id)
+        strategy = Strategy.objects.get(id=self.strategy.id)
+
+        self.assertIsNotNone(anonymous_user, 'Anonymous user was not created')
+        self.assertEqual(solution.user, anonymous_user, 'Solution user not updated to anonymous')
+        self.assertEqual(strategy.user, anonymous_user, 'Strategy user not updated to anonymous')
